a37c3a6c6f038206a81b7eb56a0770537cae3a5c
Commandtechno <68407783+Commandtechno@users.noreply.github.com>
2023-12-18 19:07:04 -0600

    gh pages

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,6 +3,5 @@
 interface ImportMetaEnv {
   readonly STRAPI_URL: string;
   readonly STRAPI_API_TOKEN: string;
-  readonly STRAPI_WEBHOOK_TOKEN: string;
-  readonly RESEND_API_TOKEN: string;
+  readonly CONTACT_API_URL: string;
 }
