88a89a47b7a80d4f1c40492791f14e8f5f580b91
Commandtechno <68407783+Commandtechno@users.noreply.github.com>
2024-02-29 12:51:57 -0600

    cloudflare turnstile

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,8 @@
 /// <reference types="astro/client" />
 
 interface ImportMetaEnv {
+  readonly CONTACT_API_URL: string;
   readonly STRAPI_URL: string;
   readonly STRAPI_API_TOKEN: string;
-  readonly CONTACT_API_URL: string;
+  readonly TURNSTILE_SITE_KEY: string;
 }
